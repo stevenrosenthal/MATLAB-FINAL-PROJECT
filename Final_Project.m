a5c3db5816f8478a465e55873bb04a8fd4624694
stevenrosenthal <31748418+stevenrosenthal@users.noreply.github.com>
2018-04-18 19:35:54 -0400

    smart link work

--- Final_Project.m
+++ Final_Project.m
@@ -1,1 +1,9 @@
 %Final Project
+clc;clear;
+filename = input('Enter filename: ','s');
+pixels = imread(filename);
+[height,width,colors]=size(pixels);
+[r,c] = find( pixels(:,:,1)> 150 & pixels(:,:,1)< 165 & pixels(:,:,2) > 195 & pixels(:,:,2) < 210 & pixels(:,:,3) > 155 & pixels(:,:,3) < 175);
+binary = false(height,width);
+binary(r,c)=1;
+imshowpair(pixels,binary,'montage');
